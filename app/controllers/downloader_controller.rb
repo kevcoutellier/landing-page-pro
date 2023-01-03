3ea3d9d96914705fb5f34faf3f30118fc3c99daf
Kevin Coutellier <kev.coutellier@gmail.com>
2023-01-03 16:09:49 +0100

    A mettre en production

---
+++ app/controllers/downloader_controller.rb
@@ -0,0 +1,10 @@
+class DownloaderController < ApplicationController
+  def download
+    pdf = WickedPdf.new.pdf_from_string(            #1
+      render_to_string('download', layout: false))  #2
+    send_data(pdf,                                  #3
+      filename: 'cv.pdf',                     #4
+      type: 'application/pdf',                      #5
+      disposition: 'attachment')                    #6
+  end
+end
